b1c9d5c02da05044ff5da9ee7881c902aad269fb
sajmir.rusi <sajmir.rusi@aizoon.it>
2017-05-07 13:07:47 +0200

    fine lezione 1 stato della app biblioteca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = '';
+  bookElements = [];
+  newBookName = '';
+  newBookContent = '';
+
+  onAddBook() {
+    this.bookElements.push({
+      type: 'book',
+      name: this.newBookName,
+      content: this.newBookContent
+    });
+  }
+
+  onAddMagazine() {
+    this.bookElements.push({
+      type: 'magazine',
+      name: this.newBookName,
+      content: this.newBookContent
+    });
+  }
 }
